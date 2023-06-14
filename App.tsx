136f1f2892916c09b56e5fa34cbdfc631fd4a5ae
Jan-Niklaas Koch <jan@sustayn.de>
2023-06-14 09:08:37 +0200

    create minimal example for useFonts error

--- App.tsx
+++ App.tsx
@@ -1,18 +1,30 @@
+import { useFonts } from 'expo-font';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+    const [fontsLoaded, error] = useFonts({
+        'montserrat-regular': require('./assets/fonts/Montserrat-Regular.ttf'),
+        'montserrat-bold': require('./assets/fonts/Montserrat-Bold.ttf'),
+        'montserrat-semibold': require('./assets/fonts/Montserrat-SemiBold.ttf'),
+        'opensans-regular': require('./assets/fonts/OpenSans-Regular.ttf'),
+        'spacemono-regular': require('./assets/fonts/SpaceMono-Regular.ttf'),
+    });
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.styledText}>Open up App.tsx to start working on your app!</Text>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    styledText: {
+        fontFamily: 'montserrat-regular',
+    },
 });
